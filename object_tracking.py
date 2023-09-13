1006893bdcddeac48dac85a6c7ecdb243ddf7d91
tarek <tarekeesa7@gmail.com>
2023-09-13 15:04:04 -0400

    first commit

---
+++ object_tracking.py
@@ -0,0 +1,54 @@
+# Python In-built packages
+from pathlib import Path
+
+# External packages
+import streamlit as st
+from ultralytics import YOLO
+
+# Local Modules
+import settings
+import helper
+
+# Setting page layout
+st.set_page_config(
+    page_title="Object Detection using YOLOv8",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+# Main page heading
+st.title("Object Detection using YOLOv8")
+
+# Sidebar
+st.sidebar.header("ML Model Config")
+
+# Model Options
+model_type = st.sidebar.radio(
+    "Select Task", ['Detection', 'Segmentation'])
+confidence = float(st.sidebar.slider(
+    "Select Model Confidence", 25, 100, 40)) / 100
+
+# Selecting Detection Or Segmentation
+if model_type == 'Detection':
+    model_path = Path(settings.DETECTION_MODEL)
+elif model_type == 'Segmentation':
+    model_path = Path(settings.SEGMENTATION_MODEL)
+
+# Load Pre-trained ML Model
+try:
+    model = YOLO(model_path)
+except Exception as ex:
+    st.error(f"Unable to load model. Check the specified path: {model_path}")
+    st.error(ex)
+
+st.sidebar.header("Image/Video Config")
+source_radio = st.sidebar.radio(
+    "Select Source", settings.SOURCES_LIST)
+
+if source_radio == settings.VIDEO:
+    helper.play_stored_video(confidence, model)
+elif source_radio == settings.YOUTUBE:
+    helper.play_youtube_video(confidence, model)
+else:
+    st.error("Please select a valid source type!")
